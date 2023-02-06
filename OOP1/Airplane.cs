692890adb7576a3729123da0474fc2da489689ab
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2023-02-06 09:25:56 +0200

    Create constructors

--- OOP1/Airplane.cs
+++ OOP1/Airplane.cs
@@ -12,5 +12,35 @@ namespace OOP1
         protected string FinishCity;
         protected Date StartDate;
         protected Date FinishDate;
+        
+        public Airplane()/*Замовчуванню*/
+        {
+            StartCity = "Kyiv";
+            FinishCity = "Budapest";
+            StartDate= new Date();
+            FinishDate = new Date();
+        }
+        public Airplane(string startCity, string finishCity, Date startDate, Date finisDate)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+            StartDate = new Date(startDate);
+            FinishDate = new Date(finisDate);
+        }
+        public Airplane(Date startDate, Date finisDate)
+        {
+            StartCity = "Kyiv";
+            FinishCity = "Budapest";
+            StartDate = new Date(startDate);
+            FinishDate = new Date(finisDate);
+        }
+        public Airplane(Airplane airplane)/*Копіювання*/
+        {
+            StartCity= airplane.StartCity;
+            FinishCity= airplane.FinishCity;
+            StartDate= new Date(StartDate);
+            FinishDate= new Date(FinishDate);
+        }
     }
+
 }
--- OOP1/Date.cs
+++ OOP1/Date.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using OOP1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,38 @@ namespace OOP1
         protected int Day;
         protected int Hours;
         protected int Minutes;
+        public Date()/*По замовчуванню*/
+        {
+            Year = 2023;
+            Month = 2;
+            Day = 3;
+            Hours = 9;
+            Minutes = 54;
+        }
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+            Minutes = minutes;
+        }
+        public Date(int year, int month, int day, int hours)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+            Minutes = 0;
+        }
+        public Date (Date date)/*Копіювання*/
+        {
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+            Hours = date.Hours;
+            Minutes = date.Minutes;
+        }
     }
+   
 }
